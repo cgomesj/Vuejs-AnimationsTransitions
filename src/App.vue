19b3f2a4ddb867dcaefd144f1492fa902aafa0ab
cgomesj <32676188+cgomesj@users.noreply.github.com>
2019-07-19 16:37:29 -0300

    animations with animate.css library

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,32 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Animations</h1>
 
-        <button @click="toggleAlert" class="btn btn-primary">Show alert</button>
+        <select class="form-control form-control-sm" v-model="selectedTransition">
+          <option value="fade">Fade</option>
+          <option value="slide">Slide</option>
+        </select>
+        <br />
+        <button @click="toggleAlert" class="btn btn-primary form-control">Show alerts</button>
 
-        <transition name="fade">
+        <transition :name="selectedTransition">
           <div v-if="show" class="alert alert-info my-2">This is a faded alert</div>
         </transition>
 
-        <transition name="slide" type="animation">
+        <transition :name="selectedTransition">
           <div v-if="show" class="alert alert-info my-2">This is a slided and faded alert</div>
         </transition>
 
-        <transition name="slide" type="animation" appear>
-          <div v-if="show" class="alert alert-info my-2">This is a slided, faded and onLoad alert</div>
+        <transition enter-active-class="animated bounce" leave-active-class="animated swing">
+          <div v-if="show" class="alert alert-info my-2">This is an animated bounce swing div</div>
         </transition>
+
+        <hr />
+
+        <div
+          v-if="show"
+          :class="animationHeadShake"
+          class="alert alert-info my-2"
+        >This is animated alert</div>
       </div>
     </div>
   </div>
@@ -25,7 +38,16 @@
 <script>
 export default {
   data() {
-    return { show: true };
+    return {
+      show: true,
+      selectedTransition: "fade"
+    };
+  },
+
+  computed: {
+    animationHeadShake() {
+      return "animated infinite headShake slow";
+    }
   },
 
   methods: {
